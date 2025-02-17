cd3411d100301f9bc9c3148b6f08269d27401bee
Chuyedi <130406303+Chuyedi@users.noreply.github.com>
2025-02-17 17:56:28 -0500

    added banner and row component

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,47 @@
+import "./Banner.css";
+import Axios from "../../Utils/axios";
+import { useEffect, useState } from "react";
+import fetchData from "../../Utils/requests";
+
+const Banner = () => {
+  const [movie, setMovie] = useState({});
+  useEffect(() => {
+     Axios.get(fetchData.getUpcommingMovies)
+      .then((res) => {
+        const index = Math.floor(Math.random() * res.data.results.length);
+        setMovie(res.data.results[index]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button play">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner__fadeBottom" />
+    </div>
+  );
+};
+
+export default Banner;
